976d55d91a6947d1c473bcbe3507ea2ced9ee708
Ardakin <ardakinskywalker@gmail.com>
2025-12-08 02:28:52 +0300

    initial commit

--- functions/main.py
+++ functions/main.py
@@ -1,13 +1,14 @@
 from firebase_functions import https_fn
 from firebase_functions.options import set_global_options
-import flask
-
-from app import app as flask_app  # app.py içindeki Flask instance
+from app import app as flask_app  # app.py içindeki Flask app
 
 set_global_options(max_instances=10)
 
-
 @https_fn.on_request()
-def helppsys_app(req: https_fn.Request) -> https_fn.Response:
+def serving(req: https_fn.Request) -> https_fn.Response:
+    """
+    Firebase Hosting'ten gelen tüm HTTP istekleri buraya düşecek.
+    Biz de isteği Flask app'ine forward edip cevabı geri döndürüyoruz.
+    """
     with flask_app.request_context(req.environ):
         return flask_app.full_dispatch_request()
